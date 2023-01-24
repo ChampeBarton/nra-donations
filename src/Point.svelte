6d046cbc6013dd3d82b16f00d9edc9049db6d5bf
champebarton <champebarton@gmail.com>
2023-01-23 16:43:27 -0800

    logging more shit

--- src/Point.svelte
+++ src/Point.svelte
@@ -143,8 +143,10 @@
                         var edge_x = document.querySelector("Canvas").getBoundingClientRect().right - 10
                         var edge_y = document.querySelector("Canvas").getBoundingClientRect().bottom - 115
 
-                        console.log(edge_x)
-                        console.log(edge_y)
+                        console.log("edge_x: " + edge_x)
+                        console.log("edge_y: " + edge_y)
+                        console.log("x: " + x)
+                        console.log("y: " + y)
 
                         var box_width = header_text_width >= change_text_width + compared_text_width
                         ? header_text_width + 30
